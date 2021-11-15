411d042287d9d380f64c5cf973e661691299690c
stanislaw.kurasiewicz <stanislaw.kurasiewicz@eleader.biz>
2021-11-15 19:26:02 +0100

    Update conda env name

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ cron-explain() {
     if [ $# -ne 1 ] ; then
         echo 'Invalid arguments. Try passing cron expression in ""'
     else
-        conda activate ${CONDA_DEFAULT_ENV}
+        conda activate homework
         script=${HOME}/CronParser/main.py
         PYTHONPATH=${HOME}/CronParser \
         python ${script} --expression="$@"
